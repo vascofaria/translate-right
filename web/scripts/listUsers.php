7ac48fdd33150f8613b61799470cfb118cb488fc
Vasco Faria <vascoffaria@gmail.com>
2019-11-13 00:52:43 +0000

    Add credentials

--- web/scripts/listUsers.php
+++ web/scripts/listUsers.php
@@ -7,17 +7,19 @@
       try {
         $host = "db.ist.utl.pt";
         $user = "ist189559";
-        $password = "abc";
+        $password = "idxi1356";
         $dbname = $user;
 
         $db = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
-        <!-- $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); -->
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
         $sql = "SELECT * FROM utilizador;";
 
         $result = $db->prepare($sql);
         $result->execute();
 
+      } catch (PDOException $e) {
+        echo("<p>ERROR: {$e->getMessage()}</p>");
       }
     ?>
   </body>
