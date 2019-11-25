8b1827ae115b47ff07bd16da91b29bd0b8454a2e
Vasco Faria <vascoffaria@gmail.com>
2019-11-25 18:53:56 +0000

    To sigma

--- web/scripts/listAreaAnomalies.php
+++ web/scripts/listAreaAnomalies.php
@@ -1,53 +1,97 @@
 <html>
   <head>
     <link rel="stylesheet" type="text/css" href="../modules/bootstrap-4.3.1-dist/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="../modules/mylib/css/general.css">
   </head>
   <body>
-    <?php
-      try {
-        $host = "db.ist.utl.pt";
-        $user = "ist189559";
-        $password = "idxi1356";
-        $dbname = $user;
+    <div id="anomalies-form">
 
-        $db = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      <div class="alert alert-success">
+        <p>Insert your area, by writing the coordinates of the left top corner (1) and right bottom corner (2).</p>
+      </div>
+      <form id="anomalies-form">
+    
+        <div class="input-group mb-3 m-anomaly-form-component">
+          <div class="input-group-prepend">
+            <span class="input-group-text" id="basic-addon1">Latitude 1:</>
+          </div>
+          <input type="text" class="form-control">
+        </div>
+    
+        <div class="input-group mb-3">
+          <div class="input-group-prepend">
+            <span class="input-group-text" id="basic-addon1">Longitude 1:</>
+          </div>
+          <input type="text" class="form-control">
+        </div>
+    
+        <div class="input-group mb-3">
+          <div class="input-group-prepend">
+            <span class="input-group-text" id="basic-addon1">Latitude 2:</>
+          </div>
+          <input type="text" class="form-control">
+        </div>
+    
+        <div class="input-group mb-3">
+          <div class="input-group-prepend">
+            <span class="input-group-text" id="basic-addon1">Longitude 2:</>
+          </div>
+          <input type="text" class="form-control">
+        </div>
 
-        $sql = "SELECT * FROM anomalia;";
+        <button class="btn btn-primary m-submit-btn" type="submit">List</button>
+      </form>
+    </div>
 
-        $result = $db->prepare($sql);
-        $result->execute();
+    <div id="anomalies-table" class="m-table">
+      <?php
+      function fillTable() {
+        try {
+          $host = "db.ist.utl.pt";
+          $user = "ist189559";
+          $password = "idxi1356";
+          $dbname = $user;
 
-        echo("<table class='table m-table'>");
-          echo("<thead class='thead-dark'>");
-            echo("<tr>");
-              echo("<th scope='col'>ID</th>");
-              echo("<th scope='col'>Zona</th>");
-              echo("<th scope='col'>Imagem</th>");
-              echo("<th scope='col'>Lingua</th>");
-              echo("<th scope='col'>TimeStamp</th>");
-              echo("<th scope='col'>Descricao</th>");
-              echo("<th scope='col'>Redacao?</th>");
-            echo("<tr/>");
-          echo("<thead/>");
-          echo("<tbody>");
-            foreach($result as $row) {
+          $db = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
+          $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+          $sql = "SELECT * FROM anomalia;";
+
+          $result = $db->prepare($sql);
+          $result->execute();
+
+          echo("<table class='table'>");
+            echo("<thead class='thead-dark'>");
               echo("<tr>");
-              echo("<td>{$row['a_id']}</td>");
-              echo("<td>{$row['a_zona']}</td>");
-              echo("<td>{$row['a_imagem']}</td>");
-              echo("<td>{$row['a_lingua']}</td>");
-              echo("<td>{$row['a_ts']}</td>");
-              echo("<td>{$row['a_descricao']}</td>");
-              echo("<td>{$row['a_tem_anomalia_redacao']}</td>");
+                echo("<th scope='col'>ID</th>");
+                echo("<th scope='col'>Zona</th>");
+                echo("<th scope='col'>Imagem</th>");
+                echo("<th scope='col'>Lingua</th>");
+                echo("<th scope='col'>TimeStamp</th>");
+                echo("<th scope='col'>Descricao</th>");
+                echo("<th scope='col'>Redacao?</th>");
               echo("<tr/>");
-            }
-          echo("<tbody/>");
-        echo("<table/>");
+            echo("<thead/>");
+            echo("<tbody>");
+              foreach($result as $row) {
+                echo("<tr>");
+                echo("<td>{$row['a_id']}</td>");
+                echo("<td>{$row['a_zona']}</td>");
+                echo("<td>{$row['a_imagem']}</td>");
+                echo("<td>{$row['a_lingua']}</td>");
+                echo("<td>{$row['a_ts']}</td>");
+                echo("<td>{$row['a_descricao']}</td>");
+                echo("<td>{$row['a_tem_anomalia_redacao']}</td>");
+                echo("<tr/>");
+              }
+            echo("<tbody/>");
+          echo("<table/>");
 
-      } catch (PDOException $e) {
-        echo("<p>ERROR: {$e->getMessage()}</p>");
+        } catch (PDOException $e) {
+          echo("<p>ERROR: {$e->getMessage()}</p>");
+        }
       }
-    ?>
+      ?>
+    </div>
   </body>
 </html>
