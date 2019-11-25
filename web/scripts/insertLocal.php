f430f42ce0bd7d38dd62c97eb1ad62e90c4368a9
QuickSoOrt <brunorafaelfariameira@gmail.com>
2019-11-25 19:16:14 +0000

    ola

--- web/scripts/insertLocal.php
+++ web/scripts/insertLocal.php
@@ -8,8 +8,8 @@
 	<form class="needs-validation" novalidate>
   		<div class="form-row">
     		<div class="col-md-4 mb-3">
-    			<label for="validationCustom01">First name</label>
-      			<input type="text" class="form-control" id="validationCustom01" placeholder="First name" value="Mark" required>
+    			<label for="validationCustom01">Local name</label>
+      			<input type="text" class="form-control" id="validationCustom01" placeholder="First name" value="" required>
       			<div class="valid-feedback">
         			Looks good!
       			</div>
@@ -17,6 +17,27 @@
  		</div>
   		<button class="btn btn-primary" type="submit">Submit form</button>
 	</form>
+
+	<script>
+		// Example starter JavaScript for disabling form submissions if there are invalid fields
+		(function() {
+  			'use strict';
+ 	 		window.addEventListener('load', function() {
+    			// Fetch all the forms we want to apply custom Bootstrap validation styles to
+    			var forms = document.getElementsByClassName('needs-validation');
+    			// Loop over them and prevent submission
+    			var validation = Array.prototype.filter.call(forms, function(form) {
+      				form.addEventListener('submit', function(event) {
+        				if (form.checkValidity() === false) {
+         	 				event.preventDefault();
+          					event.stopPropagation();
+       					}
+        				form.classList.add('was-validated');
+      				}, false);
+    			});
+  			}, false);
+		})();
+	</script>
 
 	<?php
 		try {
