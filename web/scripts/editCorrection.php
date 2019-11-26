ac8d420389e0681e7ed226148252798cf42cb49e
Nao percebo nada disto <ruilopesbaeta@gmail.com>
2019-11-26 23:34:49 +0000

    first edit bitch

--- web/scripts/editCorrection.php
+++ web/scripts/editCorrection.php
@@ -13,12 +13,14 @@
                 $user     = "ist189559";
                 $password = "idxi1356";
                 $dbname   = $user;
-
+$oldemail=$_POST['oldEmail'];
                 $email=$_POST['newEmail'];
                 $pcnro=$_POST['newPcNro'];
                 $aid=$_POST['newAId'];
 
                  echo("<p>$email</p>");
+echo("<p>$oldemail</p>");
+
 
                 $db = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
                 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -64,11 +66,12 @@
             echo("<tbody>");
               foreach($result as $row) {
                 echo("<tr><form action='' method='POST'>");
+                echo("<td><input type='hidden' name='oldEmail' value='"."{$row['u_email']}"."'></td>");
                 echo("<td><input type='text' name='newEmail' value='"."{$row['u_email']}"."'></td>");
                 echo("<td><input type='text' name='newPcNro' value='"."{$row['pc_nro']}" ."'></td>");
                 echo("<td><input type='text' name='newAId'   value='"."{$row['a_id']}"   ."'></td>");
                 echo("<td>
-                    <button class='btn btn-primary m-submit-btn' type='submit' name='submit-edit'>
+                    <button class='btn btn-primary m-submit-btn' type='submit' name='submit-edit' >
                         Edit
                     </button>
                     </td>");
