dbb32fae00335126daf3c4265836bc001506d606
Vasco Faria <vascoffaria@gmail.com>
2019-11-26 14:55:55 +0000

    ok

--- queries.sql
+++ queries.sql
@@ -1,17 +1,20 @@
--- a.
+-- 1.
+SELECT * FROM (
+  SELECT lp_latitude, lp_longitude, COUNT(*) as cnt FROM (
+    SELECT a_id, lp_latitude, lp_longitude FROM (
+      Item NATURAL JOIN (SELECT a_id, i_id FROM Incidencia)
+    )
+  ) group by lp_latitude, lp_longitude
+) HAVING cnt=max(cnt);
+
+-- 2.
 
 
--- b.
+-- 3.
 
 
--- c.
+-- 4.
 
 
--- d.
 
-
--- e.
-
-
--- f.
-
+SELECT a_id, lp_latitude, lp_longitude FROM Item NATURAL JOIN (SELECT a_id, i_id FROM Incidencia) as A;
