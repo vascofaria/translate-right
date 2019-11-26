3930d87e0adedc5d95a5461b868fc4b7d0f57468
QuickSoOrt <brunorafaelfariameira@gmail.com>
2019-11-26 23:07:30 +0000

    ola

--- web/scripts/insertItem.php
+++ web/scripts/insertItem.php
@@ -1,33 +1,82 @@
 <html>
 <head>
-	
+    <link rel="stylesheet" type="text/css" href="../modules/bootstrap-4.3.1-dist/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="../modules/mylib/css/general.css">
 </head>
 
 <body>
-	<?php
-		try {
-			$host     = "db.ist.utl.pt";
-       		$user     = "ist189559";
-        	$password = "idxi1356";
-        	$dbname   = $user;
-		
-        	$db = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
-        	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		
-        	$query = "";
+    <form 
+        class="needs-validation" 
+        novalidate 
+        style="width: 50%; height: 50%; position: absolute; left: 50%; top: 50%; transform: translate(-50%, -60%);">
+        <div class="form-group row">
+            <div class="col-sm-10">
+                <label style="width:100%; text-align: center;"><h4>Insert Item</h4></label>
+            </div>
+        </div>
+        <div class="form-group row">
+            <div class="col-sm-10">
+                <label for="validationCustom01">Location</label>
+                <input type="text" class="form-control" id="validationCustom01" placeholder="First name" value="" required>
+                <div class="valid-feedback">
+                    Looks good!
+                </div>
+            </div>
+        </div>
+        <div class="form-group row">
+            <div class="col-sm-10">
+                <label for="validationCustom01">Longitude</label>
+                <input type="text" class="form-control" id="validationCustom01" placeholder="Longitude" value="" required>
+                <div class="valid-feedback">
+                    Looks good!
+                </div>
+            </div>
+        </div>
+        <div class="form-group row">
+            <div class="col-sm-10">
+                <label for="validationCustom01">Latitude</label>
+                <input type="text" class="form-control" id="validationCustom01" placeholder="Longitude" value="" required>
+                <div class="valid-feedback">
+                    Looks good!
+                </div>
+            </div>
+        </div>
+        <div class="form-group row">
+            <div class="col-sm-10">
+                <label for="validationCustom01">Description</label>
+                <input type="text" class="form-control" id="validationCustom01" placeholder="Longitude" value="" required>
+                <div class="valid-feedback">
+                    Looks good!
+                </div>
+            </div>
+        </div>
+        <button class="btn btn-primary" type="submit">Submit form</button>
+    </form>
 
-        	$result = pg_prepare("myQuery", $query);
+    <?php
+        try {
+            $host     = "db.ist.utl.pt";
+            $user     = "ist189559";
+            $password = "idxi1356";
+            $dbname   = $user;
+        
+            $db = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
+            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+            $query = "";
 
-        	$db = null;
-		}
-		catch (PDOException $e) {
-        	echo("<p>ERROR: {$e->getMessage()}</p>");
+            $result = pg_prepare("myQuery", $query);
+
+            $db = null;
+        }
+        catch (PDOException $e) {
+            echo("<p>ERROR: {$e->getMessage()}</p>");
         }
 
         function makeQuery($description, $location, $latitude, $longitude) {
             
         }
-	?>
+    ?>
 </body>
 
 </html>
--- web/scripts/insertLocal.php
+++ web/scripts/insertLocal.php
@@ -1,94 +1,97 @@
 <html>
 <head>
-	<link rel="stylesheet" type="text/css" href="../modules/bootstrap-4.3.1-dist/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="../modules/bootstrap-4.3.1-dist/css/bootstrap.css">
     <link rel="stylesheet" type="text/css" href="../modules/mylib/css/general.css">
 </head>
 
 <body>
-	<form 
-		class="needs-validation" 
-		novalidate 
-		style="width: 50%; height: 50%; position: absolute; left: 50%; top: 50%; transform: translate(-50%, -60%);">
-  		<div class="form-group row">
+    <form 
+        class="needs-validation" 
+        novalidate 
+        style="width: 50%; height: 50%; position: absolute; left: 50%; top: 50%; transform: translate(-50%, -60%);">
+        <div class="form-group row">
             <div class="col-sm-10">
                 <label style="width:100%; text-align: center;"><h4>Insert Local</h4></label>
             </div>
         </div>
-  		<div class="form-group row">
-    		<div class="col-sm-10">
-    			<label for="validationCustom01">Local name</label>
-      			<input type="text" class="form-control" id="validationCustom01" placeholder="First name" value="" required>
-      			<div class="valid-feedback">
-        			Looks good!
-      			</div>
-    		</div>
- 		</div>
- 		<div class="form-group row">
-    		<div class="col-sm-10">
-    			<label for="validationCustom01">Longitude</label>
-      			<input type="text" class="form-control" id="validationCustom01" placeholder="Longitude" value="" required>
-      			<div class="valid-feedback">
-        			Looks good!
-      			</div>
-    		</div>
- 		</div>
- 		<div class="form-group row">
-    		<div class="col-sm-10">
-    			<label for="validationCustom01">Latitude</label>
-      			<input type="text" class="form-control" id="validationCustom01" placeholder="Longitude" value="" required>
-      			<div class="valid-feedback">
-        			Looks good!
-      			</div>
-    		</div>
- 		</div>
-  		<button class="btn btn-primary" type="submit">Submit form</button>
-	</form>
+        <div class="form-group row">
+            <div class="col-sm-10">
+                <label for="validationCustom01">Local name</label>
+                <input type="text" class="form-control" id="validationCustom01" placeholder="First name" value="" required>
+                <div class="valid-feedback">
+                    Looks good!
+                </div>
+            </div>
+        </div>
+        <div class="form-group row">
+            <div class="col-sm-10">
+                <label for="validationCustom01">Longitude</label>
+                <input type="text" class="form-control" id="validationCustom01" placeholder="Longitude" value="" required>
+                <div class="valid-feedback">
+                    Looks good!
+                </div>
+            </div>
+        </div>
+        <div class="form-group row">
+            <div class="col-sm-10">
+                <label for="validationCustom01">Latitude</label>
+                <input type="text" class="form-control" id="validationCustom01" placeholder="Longitude" value="" required>
+                <div class="valid-feedback">
+                    Looks good!
+                </div>
+            </div>
+        </div>
+        <button class="btn btn-primary" type="submit">Submit form</button>
+    </form>
 
-	<script>
-		// Example starter JavaScript for disabling form submissions if there are invalid fields
-		(function() {
-  			'use strict';
- 	 		window.addEventListener('load', function() {
-    			// Fetch all the forms we want to apply custom Bootstrap validation styles to
-    			var forms = document.getElementsByClassName('needs-validation');
-    			// Loop over them and prevent submission
-    			var validation = Array.prototype.filter.call(forms, function(form) {
-      				form.addEventListener('submit', function(event) {
-        				if (form.checkValidity() === false) {
-         	 				event.preventDefault();
-          					event.stopPropagation();
-       					}
-        				form.classList.add('was-validated');
-      				}, false);
-    			});
-  			}, false);
-		})();
-	</script>
+    <script>
+        // Example starter JavaScript for disabling form submissions if there are invalid fields
+        (function() {
+            'use strict';
+            window.addEventListener('load', function() {
+                // Fetch all the forms we want to apply custom Bootstrap validation styles to
+                var forms = document.getElementsByClassName('needs-validation');
+                // Loop over them and prevent submission
+                var validation = Array.prototype.filter.call(forms, function(form) {
+                    form.addEventListener('submit', function(event) {
+                        if (form.checkValidity() === false) {
+                            event.preventDefault();
+                            event.stopPropagation();
+                        }
+                        form.classList.add('was-validated');
+                    }, false);
+                });
+            }, false);
+        })();
+    </script>
 
-	<?php
-		try {
-			$host     = "db.ist.utl.pt";
-       		$user     = "ist189559";
-        	$password = "idxi1356";
-        	$dbname   = $user;
-		
-        	$db = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
-        	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		
-        	$query = "";
+    <?php
+        try {
+            $host     = "db.ist.utl.pt";
+            $user     = "ist189559";
+            $password = "idxi1356";
+            $dbname   = $user;
+        
+            $db = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
+            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+            $query = makeQuery();
 
-        	$result = pg_prepare("myQuery", $query);
+            $result = pg_prepare("myQuery", $query);
 
-        	$db = null;
-		}
-		catch (PDOException $e) {
-        	echo("<p>ERROR: {$e->getMessage()}</p>");
+            $result = $db->prepare($sql);
+            $result->execute();
+
+            $db = null;
+        }
+        catch (PDOException $e) {
+            echo("<p>ERROR: {$e->getMessage()}</p>");
         }
 
         function makeQuery($latitude, $longitude, $name) {
-        	
+            $query = "INSERT INTO local_publico(lp_latitude, lp_longitude, lp_nome) VALUES ($latitude, $longitude, $name);";
         }
-	?>
+    ?>
 </body>
 
 </html>
