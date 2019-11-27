1da6d6b4ee1ecd824e7b15ec9e7e85c4d8475777
Vasco Faria <vascoffaria@gmail.com>
2019-11-27 22:33:17 +0000

    Fixed Headers

--- web/scripts/listAreaAnomalies.php
+++ web/scripts/listAreaAnomalies.php
@@ -7,13 +7,10 @@
   <body>
 
     <nav class="navbar navbar-light bg-light">
-      <a class="navbar-brand" href="#">
+      <a class="navbar-brand" href="./index.html">
         <img src="../assets/right-alignment.png" width="30" height="30" class="d-inline-block align-top" alt="">
         Translate Right
       </a>
-      <form class="form-inline my-2 my-lg-0" action="./index.html">
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Go back</button>
-      </form>
       <form class="form-inline my-2 my-lg-0" action="../index.html">
         <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Log out</button>
       </form>
