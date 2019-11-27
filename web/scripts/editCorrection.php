ac4bd433830608521d3901f53b0c8267d1e47fde
Nao percebo nada disto <ruilopesbaeta@gmail.com>
2019-11-27 19:18:22 +0000

    dois edits

--- web/scripts/editCorrection.php
+++ web/scripts/editCorrection.php
@@ -1,6 +1,5 @@
 <html>
 <head>
-    <meta charset="UTF-8">
 	<link rel="stylesheet" type="text/css" href="../modules/bootstrap-4.3.1-dist/css/bootstrap.css">
     <link rel="stylesheet" type="text/css" href="../modules/mylib/css/general.css">
 </head>
@@ -15,19 +14,15 @@
                 $password = "idxi1356";
                 $dbname   = $user;
                 
-                $oldEmail=$_POST['oldEmail'];
+                $Email=$_POST['Email'];
                 $oldPcNro=$_POST['oldPcNro'];
-                $oldAId=$_POST['oldAId'];
-                $newEmail=$_POST['newEmail'];
                 $newPcNro=$_POST['newPcNro'];
-                $newAId=$_POST['newAId'];
-
-
+                $AId=$_POST['AId'];
 
                 $db = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
                 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             
-                $sql = "UPDATE correcao SET u_email='$newEmail', pc_nro=$newPcNro, a_id=$newAId WHERE u_email='$oldEmail' AND pc_nro=$oldPcNro AND a_id=$oldAId;";
+                $sql = "UPDATE correcao SET pc_nro=$newPcNro WHERE u_email='$Email' AND pc_nro=$oldPcNro AND a_id=$AId;";
 
                 $result = $db->prepare($sql);
                 $result->execute();
@@ -68,12 +63,10 @@
             echo("<tbody>");
               foreach($result as $row) {
                 echo("<tr><form action='' method='POST'>");
-                echo("<td><input type='hidden' name='oldEmail' value='"."{$row['u_email']}"."'>");
-                echo(    "<input type='text'   name='newEmail' value='"."{$row['u_email']}"."'></td>");
-                echo("<td><input type='hidden' name='oldPcNro' value='"."{$row['pc_nro']}" ."'>");
-                echo(    "<input type='text'   name='newPcNro' value='"."{$row['pc_nro']}" ."'></td>");
-                echo("<td><input type='hidden' name='oldAId'   value='"."{$row['a_id']}"   ."'>");
-                echo(    "<input type='text'   name='newAId'   value='"."{$row['a_id']}"   ."'></td>");
+                echo("<td><input type='readonly'   name='Email'    style='border:none' value='"."{$row['u_email']}"."'></td>");
+                echo("<td><input type='hidden'     name='oldPcNro'                     value='"."{$row['pc_nro']}" ."'>");
+                echo(    "<input type='text'       name='newPcNro'                     value='"."{$row['pc_nro']}" ."'></td>");
+                echo("<td><input type='readonly'   name='AId'      style='border:none' value='"."{$row['a_id']}"   ."'></td>");
                 echo("<td>
                     <button class='btn btn-primary m-submit-btn' type='submit' name='submit-edit' >
                         Edit
