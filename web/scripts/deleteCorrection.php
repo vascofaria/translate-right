76634ed7af6a24b1df31bdebc6e4e7fce528c831
NikolettaMatsur <nika.matsur1@gmail.com>
2019-11-28 18:46:55 +0000

    Make Delete Correction

--- web/scripts/deleteCorrection.php
+++ web/scripts/deleteCorrection.php
@@ -17,6 +17,80 @@
       </form>
     </nav>
 
+  
+    <?php
+        if (isset($_POST['deleteCorrection'])) {
+            try {
+                $host     = "db.ist.utl.pt";
+                $user     = "ist189559";
+                $password = "idxi1356";
+                $dbname   = $user;
+                
+                $email=$_POST['email'];
+                $pc_nro=$_POST['pc_nro'];
+
+
+                $db = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
+                $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+          
+                $query = "DELETE FROM item WHERE u_email=:email AND pc_nro=:pc_nro;";
+                $result = $db->prepare($query);
+                $result->bindValue(':email',  $_POST['email']);
+                $result->bindValue(':pc_nro',  $_POST['pc_nro']);
+                $result->execute();
+                $db = null;
+              } catch (PDOException $e) {
+                echo("<p>ERROR: {$e->getMessage()}</p>");
+              }
+        }
+    ?>
+	<?php
+		try {
+			$host     = "db.ist.utl.pt";
+       		$user     = "ist189559";
+        	$password = "idxi1356";
+        	$dbname   = $user;
+		
+        	$db = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
+        	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		
+          $sql = "SELECT * FROM correcao where u_email=:email;";
+          $result = $db->prepare($sql);
+          $result->bindValue(':email', $_COOKIE['user']);
+          $result->execute();
+
+
+          echo("<table class='table'>");
+          echo("<thead class='thead-dark'>");
+          echo("<tr>");
+          echo("<th scope='col'>Email</th>");
+          echo("<th scope='col'>Correction Proposal Number</th>");
+          echo("<th scope='col'>Anomalia ID</th>");
+          echo("<th scope='col'>Delete</th>");
+          echo("<tr/>");
+          echo("<thead/>");
+          echo("<tbody>");
+            foreach($result as $row) {
+              echo("<tr><form action='' method='POST'>");
+              echo("<td><input type='readonly'   name='Email'    readonly style='border:none' value='"."{$row['u_email']}"."'></td>");
+              echo(    "<input type='text'       name='pc_nro'                              value='"."{$row['pc_nro']}" ."'></td>");
+              echo("<td><input type='readonly'   name='AId'      readonly style='border:none' value='"."{$row['a_id']}"   ."'></td>");
+              echo("<td>
+                  <button class='btn btn-danger m-submit-btn' type='submit' name='deleteCorrection' >
+                      Delete
+                  </button>
+                  </td>");
+              echo("</form><tr/>");
+            }
+          echo("<tbody/>");
+          echo("<table/>");
+
+        	$db = null;
+		}
+		catch (PDOException $e) {
+        	echo("<p>ERROR: {$e->getMessage()}</p>");
+        }
+	?>
 
 
 </body>
