d4c1897c78e2c50f47adeec8760e9f48211caca1
Vasco Faria <vascoffaria@gmail.com>
2019-11-28 03:45:28 +0000

    Testing Area layout

--- web/scripts/listAreaAnomalies.php
+++ web/scripts/listAreaAnomalies.php
@@ -26,7 +26,6 @@
         method="POST"
         action=""
         novalidate
-        style="width: 50%; height: 50%; position: absolute; left: 50%; top: 50%; transform: translate(-50%, -60%);">
       >
       
         <div class="form-group row">
