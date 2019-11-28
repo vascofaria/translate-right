9cba1b46f6962476968e9973e3df0c3e390be0ce
NikolettaMatsur <nika.matsur1@gmail.com>
2019-11-28 15:48:58 +0000

    Fix schema by adding cascade updates and deletes and makes the query to delete locals

--- web/scripts/deleteLocal.php
+++ web/scripts/deleteLocal.php
@@ -17,7 +17,99 @@
       </form>
     </nav>
 
+    <form 
+        class="needs-validation" 
+        method="POST" 
+        novalidate 
+        style="width: 50%; height: 50%; position: absolute; left: 50%; top: 50%; transform: translate(-50%, -60%);">
+        <div class="form-group row">
+            <div class="col-sm-10">
+                <label style="width:100%; text-align: center;"><h4>Insert Local</h4></label>
+            </div>
+        </div>
+        <div class="form-group row">
+            <div class="col-sm-10">
+                <label for="validationCustom01">Local name</label>
+                <input type="text" class="form-control" id="validationCustom01" placeholder="Local name" value="" name="name" required>
+                <div class="valid-feedback">
+                    Looks good!
+                </div>
+            </div>
+        </div>
+        <div class="form-group row">
+            <div class="col-sm-10">
+                <label for="validationCustom01">Longitude</label>
+                <input type="text" class="form-control" id="validationCustom01" placeholder="Longitude" value="" name="longitude" required>
+                <div class="valid-feedback">
+                    Looks good!
+                </div>
+            </div>
+        </div>
+        <div class="form-group row">
+            <div class="col-sm-10">
+                <label for="validationCustom01">Latitude</label>
+                <input type="text" class="form-control" id="validationCustom01" placeholder="Latitude" value="" name="latitude" required>
+                <div class="valid-feedback">
+                    Looks good!
+                </div>
+            </div>
+        </div>
+        <button class="btn btn-primary" type="submit" name="submitButton">Submit form</button>
+    </form>
 
+    <script>
+        // Example starter JavaScript for disabling form submissions if there are invalid fields
+        (function() {
+            'use strict';
+            window.addEventListener('load', function() {
+                // Fetch all the forms we want to apply custom Bootstrap validation styles to
+                var forms = document.getElementsByClassName('needs-validation');
+                // Loop over them and prevent submission
+                var validation = Array.prototype.filter.call(forms, function(form) {
+                    form.addEventListener('submit', function(event) {
+                        if (form.checkValidity() === false) {
+                            event.preventDefault();
+                            event.stopPropagation();
+                        }
+                        form.classList.add('was-validated');
+                    }, false);
+                });
+            }, false);
+        })();
+    </script>
+
+    <?php
+        if (isset($_POST['submitButton'])) {
+            try {
+                $host     = "db.ist.utl.pt";
+                $user     = "ist189559";
+                $password = "idxi1356";
+                $dbname   = $user;
+            
+                $db = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
+                $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+               
+                $query = makeQuery();
+                $result = $db->prepare($query);
+
+                $result->bindValue(':latitude',  $_POST['latitude']);
+                $result->bindValue(':longitude', $_POST['longitude']);
+                $result->bindValue(':name',      $_POST['name']);
+
+                $result->execute();
+
+                $db = null;
+            }
+            catch (PDOException $e) {
+                echo("<p>ERROR: {$e->getMessage()}</p>");
+            }
+        }
+
+        function makeQuery() {
+            $query = "DELETE FROM local_publico WHERE lp_latitude=:latitude AND lp_longitude=:longitude AND lp_nome=:name);";
+            return $query;
+        }
+    ?>
 
 </body>
 </html>
