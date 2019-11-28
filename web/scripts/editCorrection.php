ad21b9dfc7192a82ce861a4066a76a6a643ecf82
Vasco Faria <vascoffaria@gmail.com>
2019-11-28 00:28:04 +0000

    utf8

--- web/scripts/editCorrection.php
+++ web/scripts/editCorrection.php
@@ -1,7 +1,8 @@
 <html>
 <head>
+  <meta charset="UTF-8">
 	<link rel="stylesheet" type="text/css" href="../modules/bootstrap-4.3.1-dist/css/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="../modules/mylib/css/general.css">
+  <link rel="stylesheet" type="text/css" href="../modules/mylib/css/general.css">
 </head>
 
 <body>
--- web/scripts/listTimeAnomalies.php
+++ web/scripts/listTimeAnomalies.php
@@ -2,6 +2,7 @@
   <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" type="text/css" href="../modules/bootstrap-4.3.1-dist/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="../modules/mylib/css/general.css">
   </head>
   <body>
 
