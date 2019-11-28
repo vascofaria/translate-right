af97e777c18bb05c315d8b6f19da6cbe289ac2f4
QuickSoOrt <brunorafaelfariameira@gmail.com>
2019-11-28 19:31:23 +0000

    ola

--- web/scripts/deleteCorrection.php
+++ web/scripts/deleteCorrection.php
@@ -17,7 +17,7 @@
       </form>
     </nav>
 
-  
+
     <?php
         if (isset($_POST['deleteCorrection'])) {
             try {
@@ -25,15 +25,11 @@
                 $user     = "ist189559";
                 $password = "idxi1356";
                 $dbname   = $user;
-                
-                $email=$_POST['email'];
-                $pc_nro=$_POST['pc_nro'];
-
 
                 $db = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
                 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-          
-                $query = "DELETE FROM item WHERE u_email=:email AND pc_nro=:pc_nro;";
+
+                $query = "DELETE FROM correcao WHERE u_email=:email AND pc_nro=:pc_nro;";
                 $result = $db->prepare($query);
                 $result->bindValue(':email',  $_POST['email']);
                 $result->bindValue(':pc_nro',  $_POST['pc_nro']);
@@ -50,13 +46,13 @@
        		$user     = "ist189559";
         	$password = "idxi1356";
         	$dbname   = $user;
-		
+
         	$db = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
         	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		
+
           $sql = "SELECT * FROM correcao where u_email=:email;";
           $result = $db->prepare($sql);
-          $result->bindValue(':email', $_COOKIE['user']);
+          $result->bindValue(':email', $_COOKIE['userID']);
           $result->execute();
 
 
@@ -72,8 +68,8 @@
           echo("<tbody>");
             foreach($result as $row) {
               echo("<tr><form action='' method='POST'>");
-              echo("<td><input type='readonly'   name='Email'    readonly style='border:none' value='"."{$row['u_email']}"."'></td>");
-              echo(    "<input type='text'       name='pc_nro'                              value='"."{$row['pc_nro']}" ."'></td>");
+              echo("<td><input type='readonly'   name='email'    readonly style='border:none' value='"."{$row['u_email']}"."'></td>");
+              echo("<td><input type='readonly'   name='pc_nro'   readonly style='border:none' value='"."{$row['pc_nro']}" ."'></td>");
               echo("<td><input type='readonly'   name='AId'      readonly style='border:none' value='"."{$row['a_id']}"   ."'></td>");
               echo("<td>
                   <button class='btn btn-danger m-submit-btn' type='submit' name='deleteCorrection' >
