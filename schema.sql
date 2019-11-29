8cbeaa313df565ad9ba75e645358046ded632666
Vasco Faria <vascoffaria@gmail.com>
2019-11-29 12:26:52 +0000

    asd

--- schema.sql
+++ schema.sql
@@ -48,7 +48,7 @@ create table anomalia (
 	a_descricao            varchar(80)  not null,
 	a_tem_anomalia_redacao boolean      not null,
 	constraint pk_anomalia primary key (a_id),
-	constraint ck_zone     check   (SUBSTRING(a_zona, 1, 3)::int8 >= 0 AND SUBSTRING(a_zona, 4, 2) = ', ' AND SUBSTRING(a_zona, 6, 3)::int8 >=0
+	constraint ck_zone     check   (SUBSTRING(a_zona, 1, 3)::int8 >= 0 AND SUBSTRING(a_zona, 4, 2) = ', ' AND SUBSTRING(a_zona, 6, 3)::int8 >=0)
 );
 
 /*
@@ -66,7 +66,7 @@ create table anomalia_traducao (
 	at_lingua2 varchar(80) not null,
 	constraint pk_anomalia_traducao primary key (a_id),
 	constraint fk_at_anomalia       foreign key (a_id) references anomalia(a_id) ON DELETE CASCADE ON UPDATE CASCADE,
-	constraint ck_zone2       check (SUBSTRING(at_zona2, 1, 3)::int8 >= 0 AND SUBSTRING(at_zona2, 4, 2) = ', ' AND SUBSTRING(at_zona2, 6, 3)::int8 >=0
+	constraint ck_zone2       check (SUBSTRING(at_zona2, 1, 3)::int8 >= 0 AND SUBSTRING(at_zona2, 4, 2) = ', ' AND SUBSTRING(at_zona2, 6, 3)::int8 >=0)
 );
 
 /*	constraint ck_zone2       check (SUBSTRING(at_zona2, 1, 3)::int8 >= -90 AND SUBSTRING(at_zona2, 1, 3)::int8 <= 90 AND SUBSTRING(at_zona2, 4, 2) = ', ' AND SUBSTRING(at_zona2, 6, 3)::int8 >=0 AND SUBSTRING(at_zona2, 6, 3)::int8 <= 180)
