99a0e6f87b3e80fc57be40da04b2f5be705b982b
Vasco Faria <vascoffaria@gmail.com>
2019-11-29 14:01:25 +0000

    Querries ok

--- web/scripts/validateForm.php
+++ web/scripts/validateForm.php
@@ -46,7 +46,7 @@
 				  $result = $db->prepare($query);
 			  	$result->execute(array($email));
 					
-				  	echo("<p>{$result['exists']}</p>")
+				  	echo("<p>{$result[0]['exists']}</p>")
 					
 				//  	//if ($result['exists']) setcookie("userQualificated", 0, time() + (86400), '/'); // 1 day
 				//  	//else setcookie("userQualificated", 1, time() + (86400), '/'); // 1 day
