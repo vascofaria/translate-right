06fb41665d18aed3bcfcd3960007610be7688868
Vasco Faria <vascoffaria@gmail.com>
2019-11-29 00:06:36 +0000

    Fixed area list

--- web/scripts/listTimeAnomalies.php
+++ web/scripts/listTimeAnomalies.php
@@ -68,7 +68,7 @@
         $db = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        $query = "SELECT anomalia.a_id, anomalia.a_zona, anomalia.a_imagem, anomalia.a_lingua, anomalia_traducao.at_lingua2, anomalia.a_ts, anomalia.a_descricao, anomalia.a_tem_anomalia_redacao
+        $query = "SELECT anomalia.a_id, anomalia.a_zona, anomalia_traducao.at_zona2, anomalia.a_imagem, anomalia.a_lingua, anomalia_traducao.at_lingua2, anomalia.a_ts, anomalia.a_descricao, anomalia.a_tem_anomalia_redacao
          FROM anomalia FULL OUTER JOIN anomalia_traducao ON anomalia.a_id = anomalia_traducao.a_id WHERE a_ts >= :since_time AND a_ts <= :to_time;";
         $result = $db->prepare($query);
         $result->execute(array($since_time, $to_time));
