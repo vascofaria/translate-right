637e7d6ce19c2543a27064bf12638dbb6938ada6
Vasco Faria <vascoffaria@gmail.com>
2019-11-29 17:27:16 +0000

    Querries ok

--- schema.sql
+++ schema.sql
@@ -54,11 +54,23 @@ create table anomalia (
 /*
 constraint ck_zone     check   (SUBSTRING(a_zona, 1, 3)::int8 >= -90 AND SUBSTRING(a_zona, 1, 3)::int8 <= 90 AND SUBSTRING(a_zona, 4, 2) = ', ' AND SUBSTRING(a_zona, 6, 3)::int8 >=0 AND SUBSTRING(a_zona, 6, 3)::int8 <= 180)
 */
+-- create function fn_Check_Zone (zone text)
+-- RETURNS int AS $$
+-- BEGIN
+--     RETURN COUNT(*) FROM anomalia WHERE a_zona = zone
+-- END; $$ LANGUAGE SQL;
 create function fn_Check_Zone (zone text)
-RETURNS int AS $$
+RETURNS int 
+AS 
+$$
+DECLARE a_count integer;
 BEGIN
-    RETURN COUNT(*) FROM anomalia WHERE a_zona = zone
-END; $$ LANGUAGE SQL;
+SELECT COUNT(*) into a_count
+FROM anomalia WHERE a_zona = zone;
+RETURN a_count;
+END 
+$$ 
+LANGUAGE SQL;
 
 create table anomalia_traducao (
 	a_id       smallint    not null unique,
