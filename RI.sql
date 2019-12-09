b0fe6b463af2a5ce09f5ccfcf56444eebb6ce480
Vasco Faria <vascoffaria@gmail.com>
2019-12-09 16:53:42 +0000

    RIs

--- RI.sql
+++ RI.sql
@@ -12,6 +12,10 @@ RETURNS integer AS $$
 	END
 $$ LANGUAGE plpgsql;*/
 
+/* okus5161 */
+
+drop table anomalia               cascade;
+drop table anomalia_traducao      cascade;
 
 create table anomalia (
 	a_id                   serial     	not null unique,
@@ -27,7 +31,7 @@ create table anomalia_traducao (
 	at_zona2   varchar(8)  not null,
 	constraint pk_anomalia_traducao primary key (a_id),
 	constraint fk_at_anomalia       foreign key (a_id) references anomalia(a_id) ON DELETE CASCADE ON UPDATE CASCADE,
-	constraint ck_zone2       check (SUBSTRING(at_zona2, 1, 3)::int8 >= 0 AND SUBSTRING(at_zona2, 4, 2) = ', ' AND SUBSTRING(at_zona2, 6, 3)::int8 >=0),
+	constraint ck_zone2       check (SUBSTRING(at_zona2, 1, 3)::int8 >= 0 AND SUBSTRING(at_zona2, 4, 2) = ', ' AND SUBSTRING(at_zona2, 6, 3)::int8 >=0)
 );
 
 insert into anomalia(a_zona, a_tem_anomalia_redacao)
