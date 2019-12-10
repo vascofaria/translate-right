0ad00ffe3d2c212a269d8e7fa02bc7cfbf3fa52c
Vasco Faria <vascoffaria@gmail.com>
2019-12-10 15:45:15 +0000

    minor

--- multiDimModel.sql
+++ multiDimModel.sql
@@ -35,12 +35,25 @@ create table d_lingua (
 	dlingua_lingua       varchar(20) not null,
 	constraint pk_d_lingua primary key (dlingua_id)
 );
+/*
+create table f_anomalia (
+	du_id       integer not null,
+	dt_id       integer not null,
+	dlocal_id   integer not null,
+	dlingua_id  integer not null,
+	constraint fk_a_dutilizador foreign key (du_id)      references d_utilizador(du_id),
+	constraint fk_a_dtempo      foreign key (dt_id)      references d_tempo(dt_id),
+	constraint fk_a_dlocal      foreign key (dlocal_id)  references d_local(dlocal_id),
+	constraint fk_a_dlingua     foreign key (dlingua_id) references d_lingua(dlingua_id)
+);*/
 
 create table f_anomalia (
-	du_id   integer not null,
-	dt_id   integer not null,
-	dlocal_id  integer not null,
-	dlingua_id  integer not null,
+	du_id           integer     not null,
+	dt_id           integer     not null,
+	dlocal_id       integer     not null,
+	dlingua_id      integer     not null,
+	fa_tipo         varchar(20) not null,
+	fa_com_proposta boolean     not null,
 	constraint fk_a_dutilizador foreign key (du_id)      references d_utilizador(du_id),
 	constraint fk_a_dtempo      foreign key (dt_id)      references d_tempo(dt_id),
 	constraint fk_a_dlocal      foreign key (dlocal_id)  references d_local(dlocal_id),
@@ -66,6 +79,7 @@ insert into d_lingua(dlingua_lingua)
 	select distinct lingua from
 		(select a_lingua as lingua from anomalia) as anom UNION (select at_lingua2 as lingua from anomalia_traducao);
 
+/*
 insert into f_anomalia(du_id, dt_id, dlocal_id, dlingua_id)
 	SELECT du_id, dt_id, dlocal_id, dlingua_id FROM
 	item NATURAL JOIN local_publico NATURAL JOIN incidencia NATURAL JOIN utilizador
@@ -73,3 +87,43 @@ insert into f_anomalia(du_id, dt_id, dlocal_id, dlingua_id)
 		WHERE du_email = u_email AND dt_dia = DATE_PART('day', a_ts) AND dt_dia_semana = DATE_PART('dow',a_ts)
 			AND dt_semana = DATE_PART('week',a_ts) AND dt_mes = DATE_PART('month',a_ts) AND dt_trimestre = DATE_PART('quarter',a_ts)
 			AND dt_ano = DATE_PART('year',a_ts)	AND dlocal_latitude = lp_latitude AND dlocal_longitude = lp_longitude AND dlingua_lingua = a_lingua;
+*/
+
+insert into f_anomalia(du_id, dt_id, dlocal_id, dlingua_id, fa_tipo, fa_com_proposta)
+	select du_id, dt_id, dlocal_id, dlingua_id, 'redacao', 1 from
+		item natural join local_publico natural join incidencia natural join utilizador
+		natural join anomalia natural join d_utilizador natural join d_tempo natural join d_local
+		natural join d_lingua natural join correcao
+			WHERE du_email = u_email and dt_dia = DATE_PART('day', a_ts) and dt_dia_semana = DATE_PART('dow',a_ts)
+				and dt_semana = DATE_PART('week',a_ts) and dt_mes = DATE_PART('month',a_ts) and dt_trimestre = DATE_PART('quarter',a_ts)
+				and dt_ano = DATE_PART('year',a_ts)	and dlocal_latitude = lp_latitude and dlocal_longitude = lp_longitude and dlingua_lingua = a_lingua
+				and a_tem_anomalia_redacao;
+
+insert into f_anomalia(du_id, dt_id, dlocal_id, dlingua_id, fa_tipo, fa_com_proposta)
+	select du_id, dt_id, dlocal_id, dlingua_id, 'redacao', 0 from
+		item natural join local_publico natural join incidencia natural join utilizador
+		natural join anomalia natural join d_utilizador natural join d_tempo natural join d_local
+		natural join d_lingua
+			WHERE du_email = u_email and dt_dia = DATE_PART('day', a_ts) and dt_dia_semana = DATE_PART('dow',a_ts)
+				and dt_semana = DATE_PART('week',a_ts) and dt_mes = DATE_PART('month',a_ts) and dt_trimestre = DATE_PART('quarter',a_ts)
+				and dt_ano = DATE_PART('year',a_ts)	and dlocal_latitude = lp_latitude and dlocal_longitude = lp_longitude and dlingua_lingua = a_lingua
+				and a_tem_anomalia_redacao and not exists(select correcao.a_id from correcao where a_id = correcao.a_id);
+
+insert into f_anomalia(du_id, dt_id, dlocal_id, dlingua_id, fa_tipo, fa_com_proposta)
+	select du_id, dt_id, dlocal_id, dlingua_id, 'traducao', 1 from
+		item natural join local_publico natural join incidencia natural join utilizador
+		natural join anomalia natural join d_utilizador natural join d_tempo natural join d_local
+		natural join d_lingua natural join anomalia_traducao natural join correcao
+			WHERE du_email = u_email and dt_dia = DATE_PART('day', a_ts) and dt_dia_semana = DATE_PART('dow',a_ts)
+				and dt_semana = DATE_PART('week',a_ts) and dt_mes = DATE_PART('month',a_ts) and dt_trimestre = DATE_PART('quarter',a_ts)
+				and dt_ano = DATE_PART('year',a_ts)	and dlocal_latitude = lp_latitude and dlocal_longitude = lp_longitude and dlingua_lingua = a_lingua;
+
+insert into f_anomalia(du_id, dt_id, dlocal_id, dlingua_id, fa_tipo, fa_com_proposta)
+	select du_id, dt_id, dlocal_id, dlingua_id, 'traducao', 0 from
+		item natural join local_publico natural join incidencia natural join utilizador
+		natural join anomalia natural join d_utilizador natural join d_tempo natural join d_local
+		natural join d_lingua natural join anomalia_traducao
+			WHERE du_email = u_email and dt_dia = DATE_PART('day', a_ts) and dt_dia_semana = DATE_PART('dow',a_ts)
+				and dt_semana = DATE_PART('week',a_ts) and dt_mes = DATE_PART('month',a_ts) and dt_trimestre = DATE_PART('quarter',a_ts)
+				and dt_ano = DATE_PART('year',a_ts)	and dlocal_latitude = lp_latitude and dlocal_longitude = lp_longitude and dlingua_lingua = a_lingua
+				and not exists(select correcao.a_id from correcao where a_id = correcao.a_id);
