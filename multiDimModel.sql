44c7d506179573bfdff5e8c50a76c2f014df97bc
Vasco Faria <vascoffaria@gmail.com>
2019-12-10 16:52:34 +0000

    minor

--- multiDimModel.sql
+++ multiDimModel.sql
@@ -137,8 +137,8 @@ insert into f_anomalia(du_id, dt_id, dlocal_id, dlingua_id, fa_tipo, fa_com_prop
 		(select dlocal_id from d_local where dlocal_latitude = lp_latitude and dlocal_longitude = lp_longitude) dlocal_id,
 		(select dlingua_id from d_lingua where dlingua_lingua = a_lingua) dlingua_id,
 		(case when exists(select a_id from anomalia_traducao where a_id = anomalia_traducao.a_id) and not a_tem_anomalia_redacao then 'traducao'
-			when not exists(select a_id from anomalia_traducao where a_id = anomalia_traducao.a_id) and a_tem_anomalia_redacao then 'redacao'
-			else 'redacao/traducao' end) fa_tipo,
+			when exists(select a_id from anomalia_traducao where a_id = anomalia_traducao.a_id) and a_tem_anomalia_redacao then 'redacao/traducao'
+			else 'redacao' end) fa_tipo,
 		(case when exists (select correcao.a_id from correcao where a_id = correcao.a_id) then true else false end) fa_com_proposta
 			from anomalia natural join incidencia natural join item
 				natural join local_publico natural join utilizador;
